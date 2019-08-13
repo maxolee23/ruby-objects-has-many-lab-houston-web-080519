e08e14137a4b461b3c26ee96c2a8b25ab3150e1f
Max Lee <maxolee23@gmail.com>
2019-08-13 16:28:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,34 +1,28 @@
+
 class Artist
 
-   attr_accessor :name
+ 	attr_accessor :name, :songs
 
-   @@songs =[]
+ 	@@all = []
 
-   def initialize(name)
-    @name = name
-    @songs = []
-  end
+ 	def initialize(name)
+		@name = name
+		@songs = []
+		@@all << self
+	end
 
-   def songs
-    @songs
-  end
+ 	def add_song(song)
+		@songs << song
+		song.artist = self
+	end
 
-   def add_song(song)
-    @songs << song
-    @@songs << song
-    song.artist = self
+ 	def add_song_by_name(song_name)
+		song = Song.new(song_name)
+		add_song(song)
+	end
 
-   end
-
-   def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    @@songs << song
-    song.artist = self
-  end
-
-   def self.song_count
-    @@songs.size
-  end
+ 	def self.song_count
+		Song.all.length
+	end
 
  end 
