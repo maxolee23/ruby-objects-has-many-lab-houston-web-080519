f74e4f203ab522af8dcd8b503738c45ddddfb80c
Max Lee <maxolee23@gmail.com>
2019-08-13 16:30:09 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,32 +1,23 @@
 class Author
   attr_accessor :name
-  @@posts = []
+  attr_reader :posts
 
    def initialize(name)
     @name = name
     @posts = []
   end
 
-   def posts
-    @posts
-  end
-
    def add_post(post)
     @posts << post
-    @@posts << post
     post.author = self
   end
 
    def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post
-    @@posts << post
-    post.author = self
+    add_post(post)
   end
 
    def self.post_count
-    @@posts.size
+    Post.all.count
   end
-
-
- end
+end
