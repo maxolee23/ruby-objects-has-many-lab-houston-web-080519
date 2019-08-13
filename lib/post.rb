5f6d7679718d4ca1027fb956a68ee455f366ce83
Max Lee <maxolee23@gmail.com>
2019-08-13 16:26:31 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,22 +1,22 @@
 
-class Post 
+class Post
   attr_accessor :title, :author
 
    @@all =[]
   def initialize(title)
     @title = title
     @@all << self
-  end 
+  end
 
    def self.all
     @@all
-  end 
+  end
 
    def author_name
     if self.author
-      self.author.name 
+      self.author.name
     else
-      nil 
-    end 
+      nil
+    end
   end
 end
