8fef6a76561f11a127f9e2c0fc9662476352ac77
Max Lee <maxolee23@gmail.com>
2019-08-13 16:28:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,34 @@
 
 class Artist
+  attr_accessor :name, :songs
 
- 	attr_accessor :name, :songs
+ @@song_count = 0
 
- 	@@all = []
+ def initialize(name) #allows access and stores artist name
+    @name = name
+    @songs = []
+end
 
- 	def initialize(name)
-		@name = name
-		@songs = []
-		@@all << self
-	end
+ def add_song(song)
+  self.songs << song #creates new instances of artist & adds it to the artist array of songs
+  song.artist = self
+  @@song_count += 1
+end
 
- 	def add_song(song)
-		@songs << song
-		song.artist = self
-	end
+ def add_song_by_name(name)
+  song = Song.new(name) #takes in an arguement for a song and then associates it with the song and artist
+  self.songs << song
+  song.artist = self
+  @@song_count += 1
+end
 
- 	def add_song_by_name(song_name)
-		song = Song.new(song_name)
-		add_song(song)
-	end
+ def songs
+  @songs
+end
 
- 	def self.song_count
-		Song.all.length
-	end
+ def self.song_count
+  @@song_count
+end
 
- end 
+ end
+ 
